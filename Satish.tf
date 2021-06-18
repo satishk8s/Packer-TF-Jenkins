fdc545c0cb82cc30045b112d4056da972c44345e
satishk8s <83827462+satishk8s@users.noreply.github.com>
2021-06-18 20:18:15 +0530

    Update Satish.tf

--- Satish.tf
+++ Satish.tf
@@ -122,9 +122,9 @@ resource "aws_instance" "web-1" {
     }
 }
 
-#output "ami_id" {
- value = "${data.aws_ami.my_ami.id}"
-}
+# #output "ami_id" {
+#  value = "${data.aws_ami.my_ami.id}"
+# }
 # !/bin/bash
 # echo "Listing the files in the repo."
 # ls -al
